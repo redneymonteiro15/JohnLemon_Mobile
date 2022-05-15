188dd3c32f8fb627a7a267681795ddc9d5e317f5
RedneyMonteiro15 <redneymonteiro15@gmail.com>
2022-05-15 01:04:51 +0100

    Menu Prinicipal
    Co-Authored-By: Jumara Fernandes <80683054+maura2020@users.noreply.github.com>

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public string scene;
+    public GameObject painel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Carregar()
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void Paused()
+    {
+        painel.SetActive(true);
+    }
+}
